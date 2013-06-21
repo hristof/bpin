7df4a2bc4535dd3392600576ef97d55a33408e57
Hristo Hristov <hristo@hristof.com>
2013-06-21 16:36:40 +0300

    c

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'kl;gdifps843nbe32432vcdfwe8';
 
 /*
 |--------------------------------------------------------------------------
