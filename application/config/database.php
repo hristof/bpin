7585585cc6bce4c50ab7df182b9a1b95f59527bc
Hristo Hristov <hristo@hristof.com>
2013-06-21 16:35:16 +0300

    DB config added

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'hristofc_bpin';
+$db['default']['password'] = 'OGTZFaH6xJ4V';
+$db['default']['database'] = 'hristofc_bpin';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
