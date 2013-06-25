e62ecda7b1763e2c5bada425c203b4ba36198f7f
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 19:23:42 +0300

    c

--- application/views/registration.php
+++ application/views/registration.php
@@ -16,7 +16,7 @@
 					<input type="email" name="email" id="email"><br> <br>
 					<input type="password" name="password" id="password"><br> <br>
 					<input type="password" name="passwordconf" id="passwordconf"><br> <br>
-					<button class="register" type="button">Register</button>
+					<button type="submit">Register</button>
 				</div>
 			</form>
 		</fieldset>
