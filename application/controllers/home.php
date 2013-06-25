4e8f674f29afdc902b9cf201e4c5ea46901494db
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 20:23:04 +0300

    c

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,7 +4,6 @@ class Home extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
-		$this->load->library('login_checker');
 		$this->load->model('home_model');
 	}
 
@@ -15,18 +14,14 @@ class Home extends CI_Controller {
 
 	public function show()
 	{
-		$data = $this->login_checker->check();
-		
-		$this->load->view('header', $data);
+		get_header();
 		$this->load->view('homepage');
 		$this->load->view('footer');
 	}
 	
 	public function register()
 	{
-		$data = $this->login_checker->check();
-		
-		$this->load->view('header', $data);
+		get_header();
 		$this->load->view('registration');
 		$this->load->view('footer');
 	}
