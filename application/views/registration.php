b5c86973ec55d9f8e9f6ef4f8a565873fe7d7a04
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 19:27:17 +0300

    c

--- application/views/registration.php
+++ application/views/registration.php
@@ -3,15 +3,15 @@
 			<legend>Registration</legend>
 			<form action="<? echo base_url().'home/register'; ?>" method="post">
 				<div class="labels">
-					<label for="firstName">Full Name :</label> <br> <br>
+					<label for="fullname">Full Name :</label> <br> <br>
 					<label for="username">Username :</label> <br>	<br>
 					<label for="email">Email :</label> <br>	<br>
 					<label for="password">Password :</label> <br> <br>
-					<label for="password">Password Again :</label> <br> <br>
+					<label for="passwordconf">Password Again :</label> <br> <br>
 				</div>
 				
 				<div class="fields">
-					<input type="text" name="name" id="name"><br> <br>
+					<input type="text" name="fullname" id="fullname"><br> <br>
 					<input type="text" name="username" id="username"><br> <br>
 					<input type="email" name="email" id="email"><br> <br>
 					<input type="password" name="password" id="password"><br> <br>
