333dcf8a1d177ac567bb82ec672a38e80617f839
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 13:02:51 +0300

    modified header.php and added some images

--- application/views/listing.php
+++ application/views/listing.php
@@ -2,16 +2,16 @@
 		<div class="listing">Pin Listing</div>
 		<div class="list">
 			<ul> <!--Тук е мястото за листинга. Не съм слагал <li> тагове , тъй като те ще се слагат от скрипта-->
-				<li> <img src="<? echo base_url().'/assets/img/image1.jpg'; ?>" /> <p class="description">Space</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image2.png'; ?>" /> <p class="description">Art</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image1.jpg'; ?>" /> <p class="description">Nature</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image2.png'; ?>" /> <p class="description">Science</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image1.jpg'; ?> /> <p class="description">Marriage</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image2.png'; ?>" /> <p class="description">Tools</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image1.jpg'; ?>" /> <p class="description">Technologies</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image2.png'; ?>" /> <p class="description">Birds</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image1.jpg'; ?>" /> <p class="description">Animals</p> </li>
-				<li> <img src="<? echo base_url().'/assets/img/image2.png'; ?>" /> <p class="description">Food</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image1.jpg'; ?>" /> <p class="description">Space</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image2.png'; ?>" /> <p class="description">Art</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image1.jpg'; ?>" /> <p class="description">Nature</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image2.png'; ?>" /> <p class="description">Science</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image1.jpg'; ?> /> <p class="description">Marriage</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image2.png'; ?>" /> <p class="description">Tools</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image1.jpg'; ?>" /> <p class="description">Technologies</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image2.png'; ?>" /> <p class="description">Birds</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image1.jpg'; ?>" /> <p class="description">Animals</p> </li>
+				<li> <img src="<? echo base_url().'assets/img/image2.png'; ?>" /> <p class="description">Food</p> </li>
 			</ul>
 			<div class="clear"> </div>
 		</div>
