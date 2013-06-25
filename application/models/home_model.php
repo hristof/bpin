efc4b013b678b1313ff106e898d18cfe65b1fdad
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 17:42:45 +0300

    Changed db table column name

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -8,7 +8,7 @@ class Home_model extends CI_Model {
 
 	public function fb_login($fb_user_id, $fb_name)
 	{
-		$this->db->query('INSERT IGNORE INTO users (reg_type, facebook_id, facebook_name) VALUES (?, ?, ?)', array(1, $fb_user_id, $fb_name));
+		$this->db->query('INSERT IGNORE INTO users (reg_type, facebook_id, fullname) VALUES (?, ?, ?)', array(1, $fb_user_id, $fb_name));
 	}
 	
 }
