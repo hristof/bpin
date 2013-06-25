cf2eb5b59582e2b75a42b1b413890640719cd4d2
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 20:29:08 +0300

    c

--- application/helpers/views_helper.php
+++ application/helpers/views_helper.php
@@ -1,5 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
 
+require_once (APPPATH . '../assets/facebook-php-sdk/facebook.php');
+
 function get_header()
 {
 	$CI = &get_instance();
