3a1866f10880de791dc4beab95877657b9c726d2
Vladislav Belichev <vbelichev@gmail.com>
2013-06-25 15:25:44 +0300

    quering DB after fb login

--- application/models/home_model.php
+++ application/models/home_model.php
@@ -6,9 +6,10 @@ class Home_model extends CI_Model {
 		parent::__construct();
 	}
 
-	public function fb_login($fb_username)
+	public function fb_login($fb_user_id)
 	{
-		
+		$data = array('reg_type' => 1, 'facebook_id' => $fb_user_id);
+		$this->db->insert('users', $data);
 	}
 	
 }
