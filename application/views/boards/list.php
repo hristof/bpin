7368365efb6aad7d1f5a930589e451ad0773aecf
Vladislav Belichev <vbelichev@gmail.com>
2013-06-26 20:16:49 +0300

    c

--- application/views/boards/list.php
+++ application/views/boards/list.php
@@ -9,9 +9,9 @@
 					<img src="<?=get_image_url($board->thumb, 200, 250);?>" />
 					<p class="description"><?=$board->title;?></p>
 					<p class="date"><?=$board->date_added;?></p>
-					<a href="<?=base_url().'pins/index/'.$board->board_id;?>">View board</a>
-					<a href="<?=base_url().'boards/edit/'.$board->board_id;?>">Edit board</a>
-					<a href="#" onclick="if (confirm('Are you sure?')) parent.location='<?=base_url().'boards/delete/'.$board->board_id;?>'">Delete board</a>
+					<a href="<?=base_url().'pins/index/'.$board->board_id;?>">View</a> |
+					<a href="<?=base_url().'boards/edit/'.$board->board_id;?>">Edit</a> |
+					<a href="#" onclick="if (confirm('Are you sure?')) parent.location='<?=base_url().'boards/delete/'.$board->board_id;?>'">Delete</a> |
 				</li>
 			<?}?>
 		</ul>
